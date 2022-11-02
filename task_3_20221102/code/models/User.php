0db0a9731916d520a07c22a62a050bf9afe4f077
a.berezhkov <dead142@mail.ru>
2022-11-02 13:47:10 +0300

    add code to task 3

---
+++ task_3_20221102/code/models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace models;
+
+class User
+{
+    public $password;
+    public $username;
+    public $email;
+    public $fname;
+    public $lname;
+
+    /**
+     * @param $password
+     * @param $username
+     * @param $email
+     * @param $fname
+     * @param $lname
+     */
+    public function __construct($password, $username, $email, $fname, $lname)
+    {
+        $this->password = $password;
+        $this->username = $username;
+        $this->email = $email;
+        $this->fname = $fname;
+        $this->lname = $lname;
+    }
+
+    public function getFullName(): string
+    {
+        return $this->fname . " " . $this->lname;
+    }
+
+    public function save(){
+        // Сохраняет пользователя
+        // return user id | null
+        return 12;
+    }
+
+
+}
