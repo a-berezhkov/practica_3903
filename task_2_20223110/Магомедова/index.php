967711d9f05693eb4ba80cdc451c6e4ee79ce281
avoskadroptime <90401339+avoskadroptime@users.noreply.github.com>
2022-11-05 11:40:00 +0300

    Доделанное и рабочее первое задание

--- task_2_20223110/Магомедова/index.php
+++ task_2_20223110/Магомедова/index.php
@@ -8,6 +8,9 @@ $a = new \php_classes\Book();
 $a->tittle = "War and Piece";
 $a->AddGenre(new \php_classes\Genre_litlle_classes\Detective());
 $a->AddGenre( new \php_classes\Genre_litlle_classes\Fantastic());
+$a->AddGenre( new \php_classes\Genre_litlle_classes\Novel());
 
 // $a->genre = [Detective Object, Fantastic Object ]
 $a->check(\php_classes\Genre_litlle_classes\Fantastic::class);
+printf(sprintf("Название книги: <b>%s</b><br>", $a->tittle));
+$a->Arr_read();
