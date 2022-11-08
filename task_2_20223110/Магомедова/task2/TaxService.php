888520d6a0805622ca1baca2cd61ff2341f50310
avoskadroptime <90401339+avoskadroptime@users.noreply.github.com>
2022-11-08 11:59:19 +0300

    Delete task_2_20223110/Магомедова/task2 directory

--- task_2_20223110/Магомедова/task2/TaxService.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Main;
-//Класс нологовой службы
-class TaxService
-{
-    public function payOut($taxAmount) {
-        printf("Уплачен налог в размере %.2f%n", taxAmount);
-    }
-}
