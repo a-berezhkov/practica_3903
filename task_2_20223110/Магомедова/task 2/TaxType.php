ac6cea5dd106e1f486a2591f89e6b66200d9ee94
avoskadroptime <90401339+avoskadroptime@users.noreply.github.com>
2022-11-08 11:58:46 +0300

    Упражнение 2 сделанное

---
+++ task_2_20223110/Магомедова/task 2/TaxType.php
@@ -0,0 +1,12 @@
+<?php //Типы налогов
+class TaxType
+{
+    public function calculateTaxFor($amount) {
+        /*switch ($amount){
+            case "VATaxType";
+            case
+        }*/
+        return $amount;
+    }
+}
+
